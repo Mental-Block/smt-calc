d24fa1b3b2b33465d6128d14b441d77d784371ad
mental-block <aaron.tibben@gmail.com>
2022-08-31 16:47:02 -0400

    fixed request interface

--- server/src/interfaces/route.ts
+++ server/src/interfaces/route.ts
@@ -1,7 +1,8 @@
 import { ValidationChain } from "express-validator"
+import { ClientRequest } from "http"
 
 export interface Route {
-    method: InstanceType<typeof Request>['method'],
+    method: InstanceType<typeof ClientRequest>['method'],
     route: string
     controller: any
     action: string
