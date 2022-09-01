80dc1fe87fe4c365f52ffbf338f4987c32c795b9
mental-block <aaron.tibben@gmail.com>
2022-09-01 18:40:30 -0400

    remove migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ const connectToDataBase = async () => {
         type: 'postgres',
         url: process.env.DATABASE_URL,
         entities: [join(__dirname, '/entities/*{.ts,.js}')],
-        migrations: [join(__dirname, '/migrations/**/*{.ts,.js}')],
+        migrations: [join(__dirname, '/migrations/*{.ts,.js}')],
         ssl: __prod__ ? true : false,
         extra: {
           ssl: __prod__ ? { rejectUnauthorized: false } : false,
@@ -33,7 +33,7 @@ const connectToDataBase = async () => {
         }
       })
       await conn.runMigrations()
-
+      
       break;
     } catch (err) {
       console.log(err);
