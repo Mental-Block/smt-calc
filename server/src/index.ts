e0bdee522d71ec86241ca7daaa1d6b492e56e1f3
mental-block <aaron.tibben@gmail.com>
2022-08-31 20:08:36 -0400

    took out prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,8 +19,8 @@ const connectToDataBase = async () => {
      const conn = await createConnection({
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        entities: __prod__ ? ["/dist/src/entities/*{.ts,.js}"] : ["/src/entities/*{.ts,.js}"],
-        migrations: __prod__ ? ["/dist/src/migrations/**/*{.ts,.js}"] : ["/src/migrations/**/*{.ts,.js}"],
+        entities: ["/dist/src/entities/*{.ts,.js}"],
+        migrations: ["/dist/src/migrations/**/*{.ts,.js}"],
         ssl: __prod__ ? { rejectUnauthorized: true } : false,
         synchronize: false,
         migrationsRun: true,
